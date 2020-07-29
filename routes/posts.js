dadf91875a2e8cd7d585be5d0e33947862398dc1
David <davidvonwachenfeldt@gmail.com>
2020-07-29 13:28:29 +0200

    Removed script that caused errors

--- routes/posts.js
+++ routes/posts.js
@@ -10,7 +10,6 @@ router.get("/", async (req, res) => {
 
 router.get("/:title", async (req, res) => {
     var title = req.params.title;
-    title = decodeURIComponent(title);
     const posts = await Post.find({ title: title }).sort({ createdAt: -1 });
 
     res.json({ success: true, data: posts })

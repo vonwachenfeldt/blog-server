b762467a436723b4c6c99a46daae3034b4e1e744
David <davidvonwachenfeldt@gmail.com>
2020-08-16 13:12:33 +0200

    added back the functionality for tags in the api

--- routes/posts.js
+++ routes/posts.js
@@ -15,7 +15,12 @@ router.get("/:title", async (req, res) => {
     res.json({ success: true, data: posts })
 });
 
-
+router.get("/tag/:tag", async (req, res) => {
+    var tag = req.params.tag;
+    tag = decodeURIComponent(tag);
+    const posts = await Post.find({ tag: tag }).sort({ createdAt: -1 });
+    res.json({ success: true, data: posts })
+});
 
 router.post("/", async (req, res) => {
     try {
